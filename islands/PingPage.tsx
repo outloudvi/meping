38fda8edc6abf33bae8cac879d4104fbba2a91df
Outvi V <git@outv.im>
2022-07-31 14:32:05 +0800

    feat: check ping response

--- islands/PingPage.tsx
+++ islands/PingPage.tsx
@@ -10,12 +10,20 @@ export default function PingPage({ pin }: CounterProps) {
   const requestPing = (pin: string) => {
     fetch(`/api/ping/${pin}`, {
       method: 'POST',
-    }).then((_) => alert('Pinged!'))
+    }).then((x) => {
+      if (x.status === 200) {
+        alert('Pinged!')
+      } else if (x.status === 404) {
+        alert('PIN not found!')
+      } else {
+        alert('Unknown error :(')
+      }
+    })
   }
 
   return (
     <div className={tw`text-center mt-3`}>
-      <div>Do you want to ping {pin}?</div>
+      <div>Do you want to ping #{pin}?</div>
       <button
         className={tw`mt-2 p-2 border-solid border-2 rounded`}
         onClick={() => requestPing(pin)}
